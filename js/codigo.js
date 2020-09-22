d542d229140278225914012d552193aee6287982
Diana Di Stefano <diana.distefano@gmail.com>
2020-09-22 15:44:42 +0200

    Creacion de HTML desde javascript

--- js/codigo.js
+++ js/codigo.js
@@ -48,10 +48,56 @@ localStorage.removeItem('');
 console.log(nombre1); */
 
 
+const serviciosWeb = [
+    {
+        tipo:"Diseño web con plantillas",
+        precio:200,
+    },
+    {
+        tipo:"Diseño web personalizado",
+        precio:300,
+    },
+    {
+        tipo:"Programación y funcionalidades",
+        precio: 500,
+    },
+];
+
+console.log(serviciosWeb);
+const [{tipo,precio}]=serviciosWeb;
+
+
+var divServicios = document.querySelector(".container-fluid.home_pricing.col-10");
+console.log(divServicios);
+
+function listadoServicios () {
+    serviciosWeb.forEach ((servicio) => {
+                
+    for (let i = 0; i<=serviciosWeb.length; i++)  {
+    let contenedorDescripcion=document.createElement('div');
+    contenedorDescripcion.classList.add(".container-fluid.home_pricing.col-10");
+    contenedorDescripcion.innerHTML=`<div class="home_pricing-opciones d-flex flex-wrap justify-content-center">
+    <div class="home_pricing-opciones-titulo col-6"><h3>${serviciosWeb[i].tipo}</h3></div>
+    <div class="home_pricing-opciones-precio col-2 d-none d-md-none d-lg-block "><h3>${serviciosWeb[i].precio}</h3></div>
+    <div class="home_pricing-opciones-checkbox col-2"><input type="checkbox" class="form-check-input" id="exampleCheck1"></div>
+    </div>`;
+    divServicios.appendChild(contenedorDescripcion);
+    }
+
+
+       
+
+     
+
+
+    })
+};
 
 
 
-function Servicios(tipo,precio)
+listadoServicios();
+
+/* function Servicios(tipo,precio)
 {
     this.tipo=tipo;
     this.precio=precio
@@ -77,5 +123,39 @@ function Calcular () {
     suma2=(suma+servicio4.precio);
 
     console.log(`El nuevo precio total es de ${suma2}`);
-}
+} */
 
+
+
+
+
+/* contenedorDescripcion.classList.add(".home_pricing-opciones-titulo.col-6");
+let descripcionServicio =document.createElement('h3'); 
+descripcionServicio.textContent=`${tipo}`;       
+contenedorDescripcion.appendChild(descripcionServicio);
+
+let contenedorPrecio=document.createElement('div');
+contenedorPrecio.classList.add(".home_pricing-opciones-precio.col-2.d-none.d-md-none.d-lg-block");
+let precioServicio=document.createElement('h3');
+contenedorPrecio.appendChild(precioServicio);
+
+let contenedorCheck=document.createElement('div');
+contenedorCheck.classList.add(".home_pricing-opciones-checkbox.col-2");
+let botonServicio=document.createElement('checkbox');
+botonServicio.classList.add(".form-check-input");
+contenedorCheck.appendChild(botonServicio);
+
+let contenedorOpciones=document.createElement('div');
+contenedorOpciones.classList.add(".home_pricing-opciones.d-flex.flex-wrap.justify-content-center");
+contenedorOpciones.appendChild(contenedorDescripcion);
+contenedorOpciones.appendChild(contenedorPrecio);
+contenedorOpciones.appendChild(contenedorCheck); */
+
+/*   let contenedorGlobal=document.createElement('div');
+contenedorGlobal.classList.add(".container-fluid.home_pricing.col-10");
+contenedorGlobal.appendChild(contenedorOpciones);
+
+console.log(contenedorGlobal); */
+
+/* var divServicios = document.querySelector(".container-fluid.home_pricing.col-10");
+divServicios.insertBefore(contenedorOpciones, divServicios.children[0]); */
