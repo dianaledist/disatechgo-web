85bfb2fba5389c0cbc4e36f75468cfcca2b675d0
Diana Di Stefano <diana.distefano@gmail.com>
2020-10-25 02:47:48 +0100

    Animaciones de titulos al hacer scroll

--- js/animation.js
+++ js/animation.js
@@ -2,8 +2,17 @@ $(document).ready(function()
 {
     $(".shape_index_circle").addClass('shape_index_circle--active');
     $(".home_screen").addClass('home_screen--active');
-    $(".home_title_subtitle").addClass('home_title_subtitle--active');
+    $(".home_title").addClass('home_title--active');
+   
 })
 
+
+$(document).scroll(function scrollTitles(){
+
+    if (window.scrollY > ($(".home_title").offset().top)) {
+    
+        $(".home_title_subtitle").addClass('home_title_subtitle--active');
+    }
+ })
 
 
